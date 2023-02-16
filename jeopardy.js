75e29e21b6daa4165ba1f79ee435a2317e6bdf8d
Casey Wittrock <caseywittrockmn@gmail.com>
2023-02-16 05:41:02 -0600

    starting data checking logic

--- jeopardy.js
+++ jeopardy.js
@@ -1,30 +1,4 @@
 "use strict";
-// categories is the main data structure for the app; it looks like this:
-
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-
-//let categories = [];
-
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
 //MODEL
 class Model {
   constructor() {
@@ -34,14 +8,25 @@ class Model {
     this.score = 0;
     this.currentClue = {};
   }
+
   addCategories(categoriesArr) {
     this.categories = categoriesArr;
   }
+
   increaseScore(clueValue) {
     this.score += clueValue;
   }
+
   updateCurrClue(clue) {
     this.currentClue = clue;
+  }
+
+  checkClueValues() {
+    for (let cat of this.categories) {
+      const clues = cat.clues;
+      for (clue of clues) {
+      }
+    }
   }
 }
 //VIEW
@@ -55,15 +40,12 @@ class View {
   makeGameBoard(cats) {
     console.log(cats);
     for (const cat of cats) {
-      console.log(cat);
       this._makeCatColumn(cat);
     }
   }
 
   _makeCatColumn(cat) {
-    console.log(cat);
     const clueArr = cat.clues;
-    console.log(cat.clues);
     let clueIndex = 0;
 
     const $newCatColumm = $("<div>").addClass("catagory-container");
@@ -82,6 +64,10 @@ class View {
     this.$gameBoard.append($newCatColumm);
   }
 
+  toggleSpinner() {
+    this.$spinContainer.toggle();
+  }
+
   _clearTable() {
     this.$gameBoard.empty();
   }
@@ -91,9 +77,13 @@ class Control {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    this.getData();
-
-    //console.log(this.randomcat);
+    this.initGame();
+  }
+  async initGame() {
+    this.view.toggleSpinner();
+    await this.getData();
+    this.view.toggleSpinner();
+    this.renderView();
   }
   async getData() {
     let categories = [];
@@ -111,20 +101,25 @@ class Control {
     console.log(categories);
     this.model.addCategories(categories);
   }
+
   async _getRandomCat() {
     const randomID = Math.floor(Math.random() * 28163);
+    //while (false) {
     let response = await axios.get("https://jservice.io/api/category", {
       params: {
         id: randomID,
       },
     });
     console.log(response.data.clues);
+    console.log(response.data.clues.length);
+    //if (response.data.clues.length)
     return response.data;
+    //}
   }
+
   renderView() {
     this.view.makeGameBoard(this.model.categories);
   }
 }
-const newGame = new Control(new Model(), new View());
 
-$("#start-reset-bttn").on("click", () => newGame.renderView());
+$("#start-reset-bttn").on("click", () => new Control(new Model(), new View()));
