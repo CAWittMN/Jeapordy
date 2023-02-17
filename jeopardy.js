cc29debad4d9831b1e894dc2692fd8aa580cd3cb
Casey Wittrock <caseywittrockmn@gmail.com>
2023-02-17 03:11:31 -0600

    working on point system and checking for correct answer

--- jeopardy.js
+++ jeopardy.js
@@ -3,7 +3,6 @@
 class Model {
   constructor() {
     this.categories = [];
-    this.player = "";
     this.numOfCategories = 6;
     this.score = 0;
     this.currentClue = {};
@@ -13,27 +12,44 @@ class Model {
     this.categories = categoriesArr;
   }
 
-  increaseScore(clueValue) {
-    this.score += clueValue;
+  increaseScore(cluePoints) {
+    this.score += cluePoints;
   }
 
-  updateCurrClue(clue) {
-    this.currentClue = clue;
+  updateCurrClue(clickedClue) {
+    console.log(clickedClue.target);
+    const clickedCatID = clickedClue.target.getAttribute("data-cat-id");
+    const clickedClueID = clickedClue.target.getAttribute("id");
+    const clickedCluePoints = Number(
+      clickedClue.target.getAttribute("data-points")
+    );
+    const currCat = this.categories.find((cat) => cat.id == clickedCatID);
+    const currClue = currCat.clues.find((clue) => clue.id == clickedClueID);
+    this.currentClue = currClue;
+    this.currentClue.value = clickedCluePoints;
+    console.log(this.currentClue.value);
   }
 
-  checkClueCount(recievedCat) {
-    return recievedCat.clues_count < 5;
+  checkAnswer(userAnswer) {
+    const lowerCaseUserAnswer = userAnswer.toLowerCase();
+    const lowerCaseAnswer = this.currentClue.answer.toLowerCase();
+    return lowerCaseAnswer.includes(lowerCaseUserAnswer);
   }
 }
+
 //VIEW
 class View {
   constructor() {
     this.$gameBoard = $("#game-board");
     this.$startResetBttn = $("#start-reset-bttn");
     this.$spinContainer = $("#spin-container");
+    this.$clueWindow = $("#clue-window");
+    this.$clueText = $("#clue-text");
+    this.$answerInput = $("#answer");
+    this.$answerButton = $("#submit-answer");
+    this.$score = $("#score");
   }
   makeGameBoard(cats) {
-    console.log(cats);
     for (const cat of cats) {
       this._makeCatColumn(cat);
     }
@@ -44,16 +60,17 @@ class View {
     let clueIndex = 0;
     let value = 200;
 
-    const $newCatColumm = $("<div>").addClass("catagory-container");
+    const $newCatColumm = $("<div>").addClass("category-container");
     const $newCatCell = $("<div>").addClass("category-cell").text(cat.title);
     $newCatColumm.append($newCatCell);
 
     _.times(5, () => {
-      const $newClueCell = $("<div>")
-        .addClass("clue")
-        .text(value)
-        .attr("id", clueArr[clueIndex].id)
-        .attr("data-points", value);
+      const workingClue = clueArr[clueIndex];
+      const $newClueCell = $("<div>").addClass("clue").text(value).attr({
+        id: workingClue.id,
+        "data-points": value,
+        "data-cat-id": workingClue.category_id,
+      });
       $newCatColumm.append($newClueCell);
       clueIndex += 1;
       value += 200;
@@ -66,61 +83,86 @@ class View {
     this.$spinContainer.toggle();
   }
 
+  toggleClueWindow() {
+    this.$clueWindow.toggle();
+  }
+
   clearTable() {
     this.$gameBoard.empty();
   }
+  initClueWindow(clueText) {
+    this.toggleClueWindow();
+    this.$clueText.text(clueText);
+  }
+  updateScore(score) {
+    this.$score.text("");
+    this.$score.text(score);
+  }
 }
+
 //CONTROL
 class Control {
   constructor(model, view) {
     this.model = model;
     this.view = view;
-    this.view.$startResetBttn.on("click", () => this.initGame());
-    this.view.$gameBoard.on("click", () => this.handleClick(event));
+    this.view.$startResetBttn.on("click", () => this._initGame());
+    this.view.$gameBoard.on("click", () => this._handleClueClick(event));
+    this.view.$answerButton.on("click", () => this._handleAnswerCheck());
   }
-  async initGame() {
+  async _initGame() {
     this.view.clearTable();
     this.view.toggleSpinner();
-    await this.getData();
+    await this._getData();
     this.view.toggleSpinner();
     this.view.makeGameBoard(this.model.categories);
   }
-  async getData() {
+  async _getData() {
     let categories = [];
     const arrOfPromises = Array.from(
       { length: this.model.numOfCategories },
       (v, i) => i
     ).map(async () => {
       let category = await this._getRandomCat();
-      console.log(this.model.checkClueCount(category));
-      while (category.clues.length < 5) {
+      while (this._checkClueCount(category)) {
         category = await this._getRandomCat();
       }
       return category;
     });
     const results = await Promise.all(arrOfPromises);
-    console.log({ results });
     categories = results;
-    console.log(categories);
     this.model.addCategories(categories);
   }
 
   async _getRandomCat() {
     const randomID = Math.floor(Math.random() * 28163);
-    //while (false) {
     let response = await axios.get("https://jservice.io/api/category", {
       params: {
         id: randomID,
       },
     });
-    console.log(response.data.clues);
-    console.log(response.data.clues.length);
-    //if (response.data.clues.length)
     return response.data;
-    //}
   }
-  handleClick(event) {
-    console.log(event.target);
+
+  _checkClueCount(recievedCat) {
+    return recievedCat.clues_count < 5;
+  }
+  _handleClueClick(event) {
+    if (event.target.classList.contains("clue")) {
+      this.model.updateCurrClue(event);
+      this.view.initClueWindow(this.model.currentClue.question);
+    }
+  }
+  _handleAnswerCheck() {
+    this.model.checkAnswer(this.view.$answerInput.val())
+      ? this._handleCorrectAnswer()
+      : this._handleIncorrectAnswer();
+  }
+  _handleCorrectAnswer() {
+    this.model.increaseScore(this.model.currentClue.value);
+    this.view.updateScore(this.model.score);
+  }
+  _handleCorrectAnswer() {
+    console.log(this.model.currentClue.answer);
   }
 }
 
